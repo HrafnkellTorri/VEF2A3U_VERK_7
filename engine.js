7280b72603e016ee64abdfc3e52961b14d8eebec
HrafnkellTorri <Kallinnhannhrabbi@hotmail.com>
2017-10-15 23:37:44 +0000

    Add files via upload

---
+++ engine.js
@@ -0,0 +1,17 @@
+document.getElementById("Svar1").addEventListener('click', function(e)
+{
+	document.getElementById("Svar1").style.backgroundColor = "red";
+	document.getElementById("Svar1").style.borderColor = "blue";
+});
+
+document.getElementById("Svar2").addEventListener('click', function(e)
+{
+	document.getElementById("Svar2").style.backgroundColor = "red";
+	document.getElementById("Svar2").style.borderColor = "blue";
+});
+
+document.getElementById("Svar3").addEventListener('click', function(e)
+{
+	document.getElementById("Svar3").style.backgroundColor = "red";
+	document.getElementById("Svar3").style.borderColor = "blue";
+});
